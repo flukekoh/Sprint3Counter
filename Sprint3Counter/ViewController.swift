0cd41268bf9b1d666d95c4f613cf4f6bbf512a19
Artem Kohan <kohanartem@gmail.com>
2022-09-11 18:51:17 +0300

    Initial Commit

---
+++ Sprint3Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sprint3Counter
+//
+//  Created by Артем Кохан on 11.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
